cb568deccf18e65c5a7790e90cefeb92cd72d277
Behrooz Khan <117090048+behroozkhan@users.noreply.github.com>
2023-10-13 20:53:46 +0500

    Home for Root

---
+++ chatapp-websocket/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+// import DrawerAppBar from '../components/AppBar'
+import Register from './Register'
+
+const Home = () => {
+  return (
+    <div>
+        <Register/>
+    </div>
+  )
+}
+
+export default Home
