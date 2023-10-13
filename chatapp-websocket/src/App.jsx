dff622e96c856be834948ea352d985ebb6846f45
Behrooz Khan <117090048+behroozkhan@users.noreply.github.com>
2023-10-13 21:06:08 +0500

    app.jsx

---
+++ chatapp-websocket/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+// import DrawerAppBar from './components/AppBar'
+import AppRouter from './components/Router'
+
+function App() {
+  return (
+    <>
+    <AppRouter/>
+    {/* <DrawerAppBar/> */}
+    </>
+  )
+}
+
+export default App
