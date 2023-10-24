9f3fc28165c6f8ce1f3765b0674e3efa7cfc3114
Behrooz Khan <117090048+behroozkhan@users.noreply.github.com>
2023-10-24 20:00:24 +0500

    randomely for future work

--- chatapp-websocket/src/pages/Home.jsx
+++ chatapp-websocket/src/pages/Home.jsx
@@ -1,9 +1,10 @@
-// import DrawerAppBar from '../components/AppBar'
+import DrawerAppBar from '../components/AppBar'
 import Register from './Register'
 
 const Home = () => {
   return (
     <div>
+      <DrawerAppBar/>
         <Register/>
     </div>
   )
