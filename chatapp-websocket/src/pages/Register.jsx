1d48a18682e747e08a5cfe3b7560dcc62047e7a0
Behrooz Khan <117090048+behroozkhan@users.noreply.github.com>
2023-10-24 20:00:06 +0500

    register with custom api

--- chatapp-websocket/src/pages/Register.jsx
+++ chatapp-websocket/src/pages/Register.jsx
@@ -1,154 +1,3 @@
-/* eslint-disable react/no-unescaped-entities */
-// import "mdb-react-ui-kit/dist/css/mdb.min.css";
-// import "@fortawesome/fontawesome-free/css/all.min.css";
-// import {
-//   MDBContainer,
-//   MDBCol,
-//   MDBRow,
-//   MDBBtn,
-//   MDBIcon,
-//   MDBInput,
-//   MDBCheckbox,
-// } from "mdb-react-ui-kit";
-
-// const Register = () => {
-//   return (
-//     <div className="container">
-//       <MDBContainer fluid className="p-3 my-5 h-custom">
-//         <MDBRow>
-//           <MDBCol col="10" md="6">
-//             <img
-//               src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-//               className="img-fluid"
-//               alt="Sample-img"
-//             />
-//           </MDBCol>
-
-//           <MDBCol col="4" md="6">
-//             <div className="d-flex flex-row align-items-center justify-content-center">
-//               <p className="lead fw-normal mb-0 me-3">Login with</p>
-
-//               <MDBBtn floating size="md" tag="a" className="me-2">
-//                 <MDBIcon fab icon="facebook-f" />
-//               </MDBBtn>
-
-//               <MDBBtn floating size="md" tag="a" className="me-2">
-//                 <MDBIcon fab icon="twitter" />
-//               </MDBBtn>
-
-//               <MDBBtn floating size="md" tag="a" className="me-2">
-//                 <MDBIcon fab icon="linkedin-in" />
-//               </MDBBtn>
-//             </div>
-
-//             <div className="divider d-flex align-items-center my-4">
-//               <p className="text-center fw-bold mx-3 mb-0">Or</p>
-//             </div>
-
-//             <MDBInput
-//               wrapperClass="mb-4"
-//               label="username"
-//               id="formControlLg"
-//               type="text"
-//               size="lg"
-//             />
-//             <MDBInput
-//               wrapperClass="mb-4"
-//               label="Email address"
-//               id="formControlLg"
-//               type="email"
-//               size="lg"
-//             />
-//             <MDBInput
-//               wrapperClass="mb-4"
-//               label="Password"
-//               id="formControlLg"
-//               type="password"
-//               size="lg"
-//             />
-
-//             <div className="d-flex justify-content-between mb-4">
-//               <MDBCheckbox
-//                 name="flexCheck"
-//                 value=""
-//                 id="flexCheckDefault"
-//                 label="Remember me"
-//               />
-//               <a href="!#">Forgot password?</a>
-//             </div>
-
-//             <div className="text-center text-md-start mt-4 pt-2">
-//               <MDBBtn className="mb-0 px-5" size="lg">
-//                 Login
-//               </MDBBtn>
-//               <p className="small fw-bold mt-2 pt-1 mb-2">
-//                 Already have an account?{" "}
-//                 <a href="#!" className="link-danger">
-//                   Login
-//                 </a>
-//               </p>
-//             </div>
-//           </MDBCol>
-//         </MDBRow>
-
-//         <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-//           <div className="text-white mb-3 mb-md-0">
-//             Copyright © 2020. All rights reserved.
-//           </div>
-
-//           <div>
-//             <MDBBtn
-//               tag="a"
-//               color="none"
-//               className="mx-3"
-//               style={{ color: "white" }}
-//             >
-//               <MDBIcon fab icon="facebook-f" size="md" />
-//             </MDBBtn>
-
-//             <MDBBtn
-//               tag="a"
-//               color="none"
-//               className="mx-3"
-//               style={{ color: "white" }}
-//             >
-//               <MDBIcon fab icon="twitter" size="md" />
-//             </MDBBtn>
-
-//             <MDBBtn
-//               tag="a"
-//               color="none"
-//               className="mx-3"
-//               style={{ color: "white" }}
-//             >
-//               <MDBIcon fab icon="google" size="md" />
-//             </MDBBtn>
-
-//             <MDBBtn
-//               tag="a"
-//               color="none"
-//               className="mx-3"
-//               style={{ color: "white" }}
-//             >
-//               <MDBIcon fab icon="linkedin-in" size="md" />
-//             </MDBBtn>
-//           </div>
-//         </div>
-//       </MDBContainer>
-//     </div>
-//   );
-// };
-
-// export default Register;
-
-
-
-
-
-
-
-
-
 
 /* eslint-disable react/no-unescaped-entities */
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
@@ -168,7 +17,7 @@ import {RegisterSchema} from "../schemas/indexSch";
 import axios from "axios";
 import Swal from "sweetalert2";
 import DrawerAppBar from "../components/AppBar";
-
+import { useNavigate } from "react-router-dom";
 const initialValues = {
   name: "",
   email: "",
@@ -177,6 +26,7 @@ const initialValues = {
 
 
 const Register = () => {
+  const navigateUser = useNavigate();
   const { values, errors, handleBlur, handleChange, handleSubmit, touched } =
   useFormik({
     initialValues,
@@ -194,12 +44,19 @@ const Register = () => {
         );
         console.log("Response:", response.data);
         if (response.status === 200) {
+          const userToken = response.data.token;
+          const userInfo = response.data.userInfo
+          localStorage.setItem('userToken', JSON.stringify(userToken))
+          localStorage.setItem('userInfo', JSON.stringify(userInfo))
           Swal.fire({
             title: "Success",
             text: "Register successful",
             icon: "success",
           });
+          console.log("Response:", response.data);
+          // localStorage.setItem('auth',JSON.stringify(response.data.token)) 
           action.resetForm();
+          navigateUser('/chat')
         } else {
           if (response.status === 404) {
             Swal.fire({
@@ -320,7 +177,7 @@ const Register = () => {
               ) : null}
             </div>
 
-            <div className="d-flex justify-content-between mb-4">
+            <div className="d-flex justify-content-between w-75 mt-4">
               <MDBCheckbox
                 name="flexCheck"
                 value=""
@@ -332,12 +189,12 @@ const Register = () => {
 
             <div className="text-center text-md-start mt-4 pt-2">
               <MDBBtn onClick={handleSubmit} className="w-25" type="submit">
-                Login
+                Register  
               </MDBBtn>
-              <p className="small fw-bold mt-2 pt-1 mb-2">
+              <p className="small fw-bold mt-4 pt-1 mb-2">
                 Don't have an account?{" "}
-                <a href="#!" className="link-danger">
-                  Register
+                <a href="/login" className="link-danger">
+                  Login
                 </a>
               </p>
             </div>
